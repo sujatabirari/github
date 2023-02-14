c9ee2c4f9f1be407f76278e251f6cbe86a64a7fa
nilesh <nilesh@192.168.0.102>
2023-02-14 16:35:32 +0530

    [Sujata]:project created

---
+++ src/githubpractice1/GithubExample1.java
@@ -0,0 +1,11 @@
+package githubpractice1;
+
+public class GithubExample1 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		System.out.println("hii");
+		System.out.println("by");
+	}
+}
