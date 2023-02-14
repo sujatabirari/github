2a764f1f9b4c1cff082caf9cd031eedd6568ced0
nilesh <nilesh@192.168.0.102>
2023-02-14 17:07:33 +0530

    added "demo 2"

--- src/githubpractice1/GithubExample1.java
+++ src/githubpractice1/GithubExample1.java
@@ -7,5 +7,7 @@ public class GithubExample1 {
 		System.out.println("hello");
 		System.out.println("hii");
 		System.out.println("by");
+		System.out.println("demo 1");
+		System.out.println("demo 2");
 	}
 }
